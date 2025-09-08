f0bee3eb4aa70653a82fe00101e2044368de8fb3
Uzair <uzair@talkwisely.io>
2025-09-08 12:40:15 +0530

    template-1

--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 
@@ -8,5 +9,6 @@ def myFn(request):
 
 
 def learnDjango(request, **kwargs):
-    values = {"course_name": "Django 5.1"}
+    d = datetime.now()
+    values = {"course_name": "Django 5.1","description":"Hey this is django templating engine default one used by django",'current_time':d}
     return render(request, 'demo/index.html', context=values)
